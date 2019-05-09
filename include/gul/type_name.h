32402a7659d4fede351018c2e8ada0265817fda3
Fini Jastrow <ulf.fini.jastrow@desy.de>
2019-05-09 09:48:52 +0200

    type_name: Fix constexpr-ness
    [why]
    After again checking the constexpr-ness of the function I found out that
    some standard-library functions are NOT constexpr despite being
    specified as such.
    So instantiation in a constexpr context failed.
    
    [how]
    Create constexpr versions of the relevant functions (in an anonymous
    namespace) and use them in type_name().
    
    [note]
    Also add missing include.
    
    Signed-off-by: Fini Jastrow <ulf.fini.jastrow@desy.de>

--- include/gul/type_name.h
+++ include/gul/type_name.h
@@ -25,6 +25,8 @@
 
 #pragma once
 
+#include "gul/string_view.h"
+
 namespace gul {
 
 /**
@@ -55,6 +57,91 @@ namespace gul {
 template <typename T>
 class FailToInstantiate;
 
+namespace {
+
+/**
+ * Returns the length of the given byte string.
+ *
+ * The length means the number of characters in a character array whose first
+ * element is pointed to by str up to and not including the first null
+ * character. The behavior is undefined if there is no null character in the
+ * character array pointed to by str.
+ *
+ * \param str Pointer to the null-terminated byte string to be examined
+ * \returns the length of the null-terminated string str
+ */
+constexpr std::size_t constexpr_strlen(const char* str)
+{
+    return *str ? 1 + constexpr_strlen(str + 1) : 0;
+}
+
+/**
+ * Create string_view from byte string.
+ *
+ * Turn a null terminated byte string into a string_view.
+ *
+ * The function differs from the normal string_view constructor
+ * in the property to be really constexpr, if the input
+ * string is also constexpr.
+ *
+ * \param str Pointer to the null-terminated byte string to be converted
+ * \returns a string_view pointing to the byte string
+ */
+constexpr string_view make_string_view(const char* str)
+{
+    return { str, constexpr_strlen(str) };
+}
+
+/**
+ * Determine whether a string starts with another string.
+ *
+ * The comparison is case sensitive. If the searched-for prefix is empty, the result is
+ * true.
+ *
+ * This variant is constexpr if the inputs are constexpr.
+ *
+ * \param haystack  The full string to be tested.
+ * \param hay       The prefix to be looked for at the beginning of \c haystack.
+ * \returns true if \c haystack starts with \c hay, false otherwise.
+ *
+ * \see starts_with(string_view, char), ends_with(string_view, string_view),
+ *      ends_with(string_view, char)
+ */
+constexpr bool constexpr_starts_with(string_view where, string_view what)
+{
+    if (what.length() == 0)
+        return true;
+    if (where.length() < what.length()
+            or where.data()[0] != what.data()[0])
+        return false;
+    // Tail recursion will be unrolled
+    return constexpr_starts_with(where.substr(1), what.substr(1));
+}
+
+/**
+ * Find the first substring equal to the given character sequence.
+ *
+ * This variant is constexpr if the inputs are constexpr.
+ *
+ * \param where The string in which to search
+ * \param what The string to search for
+ * \returns the index of the start of \c what in \c where, or string_view::npos
+ */
+constexpr std::size_t constexpr_find(string_view where, string_view what)
+{
+    if (where.length() < what.length())
+        return string_view::npos;
+    if (constexpr_starts_with(where, what))
+        return 0;
+    // This recursion can be optimized but it's not guaranteed
+    auto const n = constexpr_find(where.substr(1), what);
+    if (n == string_view::npos)
+        return n;
+    return 1 + n;
+}
+
+}
+
 /**
  * Generate a human readable string describing a type.
  *
@@ -63,6 +150,9 @@ class FailToInstantiate;
  * description of the template parameter.
  *
  * Note: `__FUNCSIG__` with MSVC.
+ *
+ * This function can only be constexpr if `__PRETTY_FUNCTION__` is constexpr.
+ * This function is not constexpr with MSVC because of language limits.
  *
  * Use like this:
  * \code
@@ -81,10 +171,13 @@ constexpr string_view type_name()
 #if defined(__GNUC__)
     // Clang returns something like "return_type function_name() [T = template_parameter; ...]"
     // GCC returns something like "return_type function_name() [with T = template_parameter]"
-    auto const s = string_view{ __PRETTY_FUNCTION__ };
-    auto const start_idx = s.find(" = ") + sizeof(" = ") - 1;
-    auto const colon_idx = s.find(';', start_idx);
-    auto const end_idx = colon_idx != gul::string_view::npos ? colon_idx - 1 : s.length() - 2;
+    auto const s = make_string_view(__PRETTY_FUNCTION__);
+    auto const start_idx = constexpr_find(s, make_string_view(" = ")) + 3; // len(" = ") == 3
+    auto const colon_idx = constexpr_find(s.substr(start_idx), make_string_view(";"));
+    auto const end_idx =
+        (colon_idx != string_view::npos)
+        ? start_idx + colon_idx - 1
+        : s.length() - 2; // len("]\0") == 2
     return s.substr(start_idx, end_idx - start_idx + 1);
 
 #elif defined(_MSC_VER)
