1c3b0ac60b9b1b9cc7ca82a45764f17921294fc6
Fini Jastrow <ulf.fini.jastrow@desy.de>
2019-05-23 07:19:18 +0200

    string_util: Remove obsolete declarations
    [why]
    With commit
    
      ed40a06 Break string_util compilation unit into parts for escape/unescape,
              starts_with/ends_with, and replace
    
              The only code remaining in string_util.h/cc is the definition of
              our default whitespace.
    
    everything but the whitespace definition should have been removed.
    But we had two (idential) declarations of escape() and unescape(),
    (and one valid definition in escape.cc).
    
    Signed-off-by: Fini Jastrow <ulf.fini.jastrow@desy.de>

--- include/gul/string_util.h
+++ include/gul/string_util.h
@@ -45,52 +45,6 @@ namespace gul {
  */
 extern const string_view default_whitespace_characters;
 
-/**
- * Create a new string that looks like an ASCII-only C string literal of the input string.
- * This is achieved by replacing all non-printable and non-ASCII characters with a hex
- * code escape in the form `\x01`.
- *
- * A few special cases are implemented to give more readable representations for very
- * common control characters, and of course backslash and double quotes are escaped as
- * well:
- *
- *     CR   ->  \r
- *     NL   ->  \n
- *     TAB  ->  \t
- *     \    ->  \\
- *     "    ->  \"
- *
- * <h4>Example</h4>
- * \code
- * std::cout << escape("Zwei\tFlüsse\nfließen ins Meer.") << "\n";
- * \endcode
- * Output (assuming that the string literal was in Latin-1 encoding):
- *
- *     Zwei\tFl\xfcsse\nflie\xdfen ins Meer.
- *
- * \note
- * The hexadecimal escape always uses two digits. This is different from the C/C++
- * standard, where it can be an arbitrary number of digits. The standard's way makes it
- * impossible to have any hex digit after a hex escape, e.g. `"\x200"` is invalid and not
- * equal to `" 0"` from the standard's point of view.
- *
- * \param in The input string.
- *
- * \returns a new string that contains only ASCII characters.
- */
-std::string escape(const std::string& in);
-
-/**
- * Evaluate a string with escaped characters to get the original string back.
- * Does only know the escape sequences used by gul::escape() and can be
- * used as in inverse function.
- *
- * \param in The string with escape sequences
- *
- * \return A new string where the sequences have been evaluated
- */
-std::string unescape(const std::string& in);
-
 } /* namespace gul */
 
 /* vim:set noexpandtab softtabstop=4 tabstop=4 shiftwidth=4 textwidth=90 cindent: */
