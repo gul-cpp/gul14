dda82803e4ccdbb9a01438120bb5c61312cd1839
Lars Froehlich <lars.froehlich@desy.de>
2019-07-29 11:58:33 +0200

    to_number: Make to_number() noexcept and return an optional<ValueType>; improve floating-point conversion
    [why]
    This is an alternative implementation to the previous one that
    relied on exceptions to communicate conversion errors. When parsing
    text, to_number() might potentially be used on a huge number of strings
    that are not valid numbers. According to the guideline "use exceptions
    only for exceptional cases", it could be argued that the implementation
    with optional<> should be preferred. In any case, it is expected to be
    faster for invalid input.
    
    Additionally, the commit improves the implementation of the
    floating-point conversion and adds more test cases.

--- include/gul/to_number.h
+++ include/gul/to_number.h
@@ -23,9 +23,11 @@
 #pragma once
 
 #include "gul/string_view.h"
+#include "gul/internal.h"
 
 namespace gul {
 
+/// \cond HIDE_SYMBOLS
 namespace detail {
 
 constexpr inline bool is_digit(char c) noexcept
@@ -34,31 +36,31 @@ constexpr inline bool is_digit(char c) noexcept
 }
 
 template <typename NumberType>
-constexpr inline NumberType to_unsigned_integer(gul::string_view str)
+constexpr inline optional<NumberType> to_unsigned_integer(gul::string_view str) noexcept
 {
 #ifndef __GNUC__
     constexpr NumberType max_tenth = std::numeric_limits<NumberType>::max() / 10;
 #endif
 
     if (str.empty())
-        throw std::invalid_argument("Cannot convert empty string to number");
+        return nullopt;
 
     NumberType result = 0;
 
     for (char c : str)
     {
         if (!is_digit(c))
-            throw std::invalid_argument("Invalid character in number");
+            return nullopt;
 
 #ifdef __GNUC__
         if (__builtin_mul_overflow(result, NumberType{ 10 }, &result))
-            throw std::out_of_range("Exceeds the range of the output type");
+            return nullopt;
 
         if (__builtin_add_overflow(result, static_cast<NumberType>(c - '0'), &result))
-            throw std::out_of_range("Exceeds the range of the output type");
+            return nullopt;
 #else
         if (result > max_tenth)
-            throw std::out_of_range("Exceeds the range of the output type");
+            return nullopt;
 
         result *= 10;
 
@@ -66,26 +68,61 @@ constexpr inline NumberType to_unsigned_integer(gul::string_view str)
 
         result += c - '0';
         if (result < last)
-            throw std::out_of_range("Exceeds the range of the output type");
+            return nullopt;
 #endif
     }
 
     return result;
 }
 
-template <typename NumberType>
-constexpr inline NumberType to_unsigned_float(gul::string_view str)
+constexpr optional<int> parse_exponent(string_view str) noexcept
 {
-    gul::string_view str_exponent;
+    bool negative = false;
 
+    switch (str.front())
+    {
+    case '+':
+        str.remove_prefix(1);
+        break;
+    case '-':
+        str.remove_prefix(1);
+        negative = true;
+        break;
+    default:
+        break;
+    }
+
+    auto opt_exp = to_unsigned_integer<int>(str);
+
+    if (!opt_exp)
+        return nullopt;
+
+    if (negative)
+        return -opt_exp.value();
+    else
+        return opt_exp.value();
+}
+
+template <typename NumberType>
+constexpr inline optional<NumberType> to_unsigned_float(gul::string_view str) noexcept
+{
+    int exponent = 0;
     auto e_pos = str.find_first_of("eE");
     if (e_pos != str.npos)
     {
         if (e_pos + 1 == str.size())
-            throw std::invalid_argument("Invalid exponent");
+            return nullopt;
 
-        str_exponent = str.substr(e_pos + 1);
+        auto str_exponent = str.substr(e_pos + 1);
+
         str = str.substr(0, e_pos);
+
+        auto opt_exp = detail::parse_exponent(str_exponent);
+
+        if (!opt_exp)
+            return nullopt;
+
+        exponent = opt_exp.value();
     }
 
     gul::string_view str_before_point{ str };
@@ -99,101 +136,131 @@ constexpr inline NumberType to_unsigned_float(gul::string_view str)
     }
 
     if (str_before_point.empty() && str_after_point.empty())
-        throw std::invalid_argument("Invalid number");
+        return nullopt;
 
+    NumberType digit_value{ 1 };
     NumberType result{ 0 };
 
-    if (!str_before_point.empty())
+    if (str_before_point.empty())
     {
-        result = 
-            static_cast<NumberType>( to_unsigned_integer<unsigned long long>(str_before_point) );
+        digit_value = std::pow(NumberType(10), exponent - 1);
+    }
+    else
+    {
+        // Try optimized integer conversion if the number fits into an unsigned long long.
+        if (str_before_point.size() <= std::numeric_limits<unsigned long long>::digits10)
+        {
+            digit_value = std::pow(NumberType(10), exponent);
+
+            auto opt = to_unsigned_integer<unsigned long long>(str_before_point);
+            if (!opt)
+                return nullopt;
+            result = opt.value() * digit_value;
+
+            digit_value *= NumberType(0.1);
+        }
+        else
+        {
+            digit_value = std::pow(NumberType(10), exponent + str_before_point.size() - 1);
+
+            for (char c : str_before_point)
+            {
+                if (!is_digit(c))
+                    return nullopt;
+
+                result += (c - '0') * digit_value;
+                digit_value *= NumberType(0.1);
+            }
+        }
     }
 
     if (!str_after_point.empty())
     {
-        result +=
-            static_cast<NumberType>( to_unsigned_integer<unsigned long long>(str_after_point) )
-            *
-            std::pow(static_cast<NumberType>(0.1), static_cast<int>(str_after_point.size()));
-    }
-
-    if (!str_exponent.empty())
-    {
-        int exponent = 0;
-
-        switch (str_exponent.front())
+        for (char c : str_after_point)
         {
-        case '+':
-            str_exponent.remove_prefix(1);
-            exponent = to_unsigned_integer<int>(str_exponent);
-            break;
-        case '-':
-            str_exponent.remove_prefix(1);
-            exponent = -to_unsigned_integer<int>(str_exponent);
-            break;
-        default:
-            exponent = to_unsigned_integer<int>(str_exponent);
-            break;
-        }
+            if (!is_digit(c))
+                return nullopt;
 
-        result *= std::pow(static_cast<NumberType>(10), exponent);
+            result += (c - '0') * digit_value;
+            digit_value *= NumberType(0.1);
+        }
     }
+
+    if (!std::isfinite(result))
+        return nullopt;
 
     return result;
 }
 
 } // namespace detail
+/// \endcond
 
 
 /**
  * Convert an ASCII string_view into a number.
- * This function parses the ASCII representation of a number (e.g. "123" or
- * "1.3e10") into an integer or floating-point number. It therefore shares
- * many characteristics with std::atoi(), std::stod() and the like, but has
- * follows its own set of design goals:
- * - Its input type is string_view: No null-termination and no temporary
- *   std::string are required for the conversion.
- * - Errors are reported by throwing std::invalid_argument or
-     std::out_of_range.
- * - The function does not allocate.
- * - Performance should in most cases be better than and in few cases not
- *   much worse than std::sto[ildf].
- * - to_number<>() can be used in a constexpr context *if* the compiler and
- *   standard library support this. Support for floating-point numbers may
- *   lack even if integer conversion works.
+ * This function parses the ASCII representation of a number (e.g. "123" or "1.3e10") into
+ * an optional integer or floating-point number.
+ *
+ * \code
+ * gul::optional<int> result = gul::to_number<int>("42");
+ *
+ * if (result)
+ *     std::cout << "The answer is " << result.value() << ".\n";
+ * \endcode
  * 
+ * <h4>Design Goals</h4>
+ * 
+ * to_number() shares many characteristics with std::atoi(), std::stod() and the like, but
+ * follows its own set of design goals:
+ * - Its input type is string_view: No null-termination and no temporary std::string are
+ *   required for the conversion.
+ * - Conversion errors are reported by returning a gul::optional without a value.
+ * - The function does not allocate.
+ * - Performance should in most cases be better than and in few cases not much worse than
+ *   std::sto[ildf].
+ * - to_number<>() can be used in a constexpr context *if* the compiler and standard
+ *   library support this. Support for floating-point numbers may lack even if integer
+ *   conversion works.
+ *
  * <h4>Input Format</h4>
  *
- * The allowed number format depends on the chosen numeric output type.
- * Unsigned integral types accept only digits ("123", "042"=42), signed
- * integral types allow for a leading minus sign as well ("-42").
- * Floating-point types additionally recognize a decimal point ("1.2",
- * ".5", "12.") and exponential notation using a small or capital "e"
- * ("12e5", "4.2e1", ".2e-4", "2.E5").
- * Surrounding whitespace is *not* ignored, but treated like an invalid
- * character. Aside from these considerations, to_number() should accept
- * all input that can be parsed by C++17's from_chars(). It could, in fact,
- * be implemented in terms of it.
+ * The allowed number format depends on the chosen numeric output type. Unsigned integral
+ * types accept only digits ("123", "042"=42), signed integral types allow for a leading
+ * minus sign as well ("-42"). Floating-point types additionally recognize a decimal point
+ * ("1.2", ".5", "12.") and exponential notation using a small or capital "e" ("12e5",
+ * "4.2e1", ".2e-4", "2.E5"). The behavior with surrounding whitespace is *undefined*, so
+ * it should be removed before passing input to this function. Apart from these
+ * considerations, to_number() should accept all input that can be parsed by C++17's
+ * from_chars(). It could, in fact, be implemented with it.
+ *
+ * \param str  The string to be converted into a number.
+ * \returns a gul::optional that contains the number if the conversion was successful. If
+ *          there was a conversion error, the return value is empty.
+ *
+ * \note
+ * This function has different overloads for unsigned integers, signed integers, and
+ * floating-point types.
+ *
+ * \since
+ * GUL 1.6
  */
+// Overload for unsigned integer types.
 template <typename NumberType,
           std::enable_if_t<std::is_integral<NumberType>::value &&
                            std::is_unsigned<NumberType>::value, int> = 0>
-constexpr inline NumberType to_number(gul::string_view str)
+constexpr inline optional<NumberType> to_number(gul::string_view str) noexcept
 {
     return detail::to_unsigned_integer<NumberType>(str);
 }
 
-/**
- * \overload
- * This is the overload for signed integer types.
- */
+// Overload for signed integer types.
 template <typename NumberType,
           std::enable_if_t<std::is_integral<NumberType>::value &&
                            std::is_signed<NumberType>::value, int> = 0>
-constexpr inline NumberType to_number(gul::string_view str)
+constexpr inline optional<NumberType> to_number(gul::string_view str) noexcept
 {
     if (str.empty())
-        throw std::invalid_argument("Cannot convert empty string to number");
+        return nullopt;
 
     if (str.front() == '-')
     {
@@ -202,35 +269,39 @@ constexpr inline NumberType to_number(gul::string_view str)
             static_cast<UnsignedT>(std::numeric_limits<NumberType>::max()) + 1;
 
         str.remove_prefix(1);
+        
         auto result = detail::to_unsigned_integer<UnsignedT>(str);
-        if (result == max_abs_negative_value)
-            return std::numeric_limits<NumberType>::lowest();
-        else if (result > max_abs_negative_value)
-            throw std::out_of_range("Exceeds the range of the output type");
+        if (!result)
+            return nullopt;
 
-        return -static_cast<NumberType>(result);
+        if (result.value() == max_abs_negative_value)
+            return std::numeric_limits<NumberType>::lowest();
+        else if (result.value() > max_abs_negative_value)
+            return nullopt;
+
+        return -static_cast<NumberType>(result.value());
     }
 
     return detail::to_unsigned_integer<NumberType>(str);
 }
 
-/**
- * \overload
- * This is the overload for floating-point types.
- */
+// Overload for floating-point types.
 template <typename NumberType,
     std::enable_if_t<std::is_floating_point<NumberType>::value, int> = 0>
-constexpr inline NumberType to_number(gul::string_view str)
+constexpr inline optional<NumberType> to_number(gul::string_view str) noexcept
 {
     if (str.empty())
-        throw std::invalid_argument("Cannot convert empty string to number");
+        return nullopt;
 
     if (str.front() == '-')
     {
         str.remove_prefix(1);
-        return -detail::to_unsigned_float<NumberType>(str);
+        auto result = detail::to_unsigned_float<NumberType>(str);
+        if (!result)
+            return nullopt;
+        return -result.value();
     }
-    
+
     return detail::to_unsigned_float<NumberType>(str);
 }
 
--- tests/test_to_number.cc
+++ tests/test_to_number.cc
@@ -23,76 +23,125 @@
 #include "gul/catch.h"
 #include "gul.h"
 
+using namespace Catch::Matchers;
 using gul::to_number;
 
 TEST_CASE("to_number(): Integer types", "[to_number]")
 {
-    REQUIRE(to_number<char>("0") == 0);
-    REQUIRE(to_number<signed char>("127") == 127);
-    REQUIRE(to_number<signed char>("-128") == -128);
-    REQUIRE(to_number<unsigned char>("255") == 255);
-    REQUIRE(to_number<int16_t>("32767") == 32767);
-    REQUIRE(to_number<int16_t>("-32768") == -32768);
-    REQUIRE(to_number<uint16_t>("65535") == 65535);
-    REQUIRE(to_number<int>("42") == 42);
-    REQUIRE(to_number<int>("-42") == -42);
-    REQUIRE(to_number<int32_t>("2147483647") == 2147483647);
-    REQUIRE(to_number<int32_t>("-2147483648") == -2147483648LL);
-    REQUIRE_THROWS_AS(to_number<int>(""), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<int>(" 42"), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<int>("42 "), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<signed char>("128"), std::out_of_range);
-    REQUIRE_THROWS_AS(to_number<signed char>("-129"), std::out_of_range);
-    REQUIRE_THROWS_AS(to_number<unsigned char>("256"), std::out_of_range);
-    REQUIRE_THROWS_AS(to_number<int16_t>("32768"), std::out_of_range);
-    REQUIRE_THROWS_AS(to_number<int16_t>("-32769"), std::out_of_range);
-    REQUIRE_THROWS_AS(to_number<int32_t>("2147483648"), std::out_of_range);
-    REQUIRE_THROWS_AS(to_number<int32_t>("-2147483649"), std::out_of_range);
+    REQUIRE(to_number<char>("0").value() == 0);
+    REQUIRE(to_number<signed char>("127").value() == 127);
+    REQUIRE(to_number<signed char>("-128").value() == -128);
+    REQUIRE(to_number<unsigned char>("255").value() == 255);
+    REQUIRE(to_number<int16_t>("32767").value() == 32767);
+    REQUIRE(to_number<int16_t>("-32768").value() == -32768);
+    REQUIRE(to_number<uint16_t>("65535").value() == 65535);
+    REQUIRE(to_number<int>("42").value() == 42);
+    REQUIRE(to_number<int>("-42").value() == -42);
+    REQUIRE(to_number<int32_t>("2147483647").value() == 2147483647);
+    REQUIRE(to_number<int32_t>("-2147483648").value() == -2147483648LL);
+    REQUIRE(to_number<int>("").has_value() == false);
+    REQUIRE(to_number<int>(" 42").has_value() == false);
+    REQUIRE(to_number<int>("42 ").has_value() == false);
+    REQUIRE(to_number<signed char>("128").has_value() == false);
+    REQUIRE(to_number<signed char>("-129").has_value() == false);
+    REQUIRE(to_number<unsigned char>("256").has_value() == false);
+    REQUIRE(to_number<int16_t>("32768").has_value() == false);
+    REQUIRE(to_number<int16_t>("-32769").has_value() == false);
+    REQUIRE(to_number<int32_t>("2147483648").has_value() == false);
+    REQUIRE(to_number<int32_t>("-2147483649").has_value() == false);
 }
 
-TEMPLATE_TEST_CASE("to_number(): Floating point types", "[to_number]", float)
+TEMPLATE_TEST_CASE("to_number(): Floating point types", "[to_number]", float, double)
 {
-    REQUIRE(to_number<TestType>("0") == 0.0);
-    REQUIRE(to_number<TestType>("12") == 12.0);
-    REQUIRE(to_number<TestType>("-12") == -12.0);
-    REQUIRE(to_number<TestType>("0.125") == 0.125);
-    REQUIRE(to_number<TestType>("-0.125") == -0.125);
-    REQUIRE(to_number<TestType>("5.") == 5.0);
-    REQUIRE(to_number<TestType>("-5.") == -5.0);
-    REQUIRE(to_number<TestType>(".5") == 0.5);
-    REQUIRE(to_number<TestType>("-.5") == -0.5);
-    REQUIRE(to_number<TestType>("123456.654321") == Approx(123456.654321));
-    REQUIRE(to_number<TestType>("1e2") == 100);
-    REQUIRE(to_number<TestType>("1e+2") == 100);
-    REQUIRE(to_number<TestType>("1.e2") == 100);
-    REQUIRE(to_number<TestType>("1.e+2") == 100);
-    REQUIRE(to_number<TestType>("1e-2") == static_cast<TestType>(0.01));
-    REQUIRE(to_number<TestType>("-1e2") == -100);
-    REQUIRE(to_number<TestType>("-1e+2") == -100);
-    REQUIRE(to_number<TestType>("-1.e2") == -100);
-    REQUIRE(to_number<TestType>("-1.e+2") == -100);
-    REQUIRE(to_number<TestType>("-1e-2") == static_cast<TestType>(-0.01));
-    REQUIRE(to_number<TestType>("1E2") == 100);
-    REQUIRE(to_number<TestType>("1E+2") == 100);
-    REQUIRE(to_number<TestType>("1E-2") == static_cast<TestType>(0.01));
-    REQUIRE(to_number<TestType>(".1e2") == 10);
-    REQUIRE(to_number<TestType>(".1e+2") == 10);
-    REQUIRE(to_number<TestType>(".1e-2") == static_cast<TestType>(0.001));
-    REQUIRE(to_number<TestType>("0.1e2") == 10);
-    REQUIRE(to_number<TestType>("0.1e+2") == 10);
-    REQUIRE(to_number<TestType>("0.1e-2") == static_cast<TestType>(0.001));
-    REQUIRE(to_number<TestType>("5e-0") == 5);
-    REQUIRE_THROWS_AS(to_number<TestType>(""), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<TestType>(" 0.1"), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<TestType>("0.1 "), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<TestType>("."), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<TestType>("-."), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<TestType>("- .1"), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<TestType>("- .1"), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<TestType>("1.2e"), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<TestType>("1e."), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<TestType>("1e+"), std::invalid_argument);
-    REQUIRE_THROWS_AS(to_number<TestType>("1e-"), std::invalid_argument);
+    REQUIRE_THAT(to_number<TestType>("0").value(), WithinULP(TestType(0), 1));
+    REQUIRE_THAT(to_number<TestType>("12").value(), WithinULP(TestType(12), 1));
+    REQUIRE_THAT(to_number<TestType>("-12").value(), WithinULP(TestType(-12.0), 1));
+    REQUIRE_THAT(to_number<TestType>("0.125").value(), WithinULP(TestType(0.125), 1));
+    REQUIRE_THAT(to_number<TestType>("-0.125").value(), WithinULP(TestType(-0.125), 1));
+    REQUIRE_THAT(to_number<TestType>("5.").value(), WithinULP(TestType(5), 1));
+    REQUIRE_THAT(to_number<TestType>("-5.").value(), WithinULP(TestType(-5), 1));
+    REQUIRE_THAT(to_number<TestType>(".5").value(), WithinULP(TestType(0.5), 1));
+    REQUIRE_THAT(to_number<TestType>("-.5").value(), WithinULP(TestType(-0.5), 1));
+    REQUIRE_THAT(to_number<TestType>("123456.654321").value(),
+                 WithinULP(TestType(123456.654321), 1));
+    REQUIRE_THAT(to_number<TestType>("123456789012345678901234567890").value(),
+                 WithinULP(TestType(123456789012345678901234567890.0), 1));
+    REQUIRE_THAT(to_number<TestType>("1e2").value(), WithinULP(TestType(100), 1));
+    REQUIRE_THAT(to_number<TestType>("1e+2").value(), WithinULP(TestType(100), 1));
+    REQUIRE_THAT(to_number<TestType>("1.e2").value(), WithinULP(TestType(100), 1));
+    REQUIRE_THAT(to_number<TestType>("1.e+2").value(), WithinULP(TestType(100), 1));
+    REQUIRE_THAT(to_number<TestType>("1e-2").value(), WithinULP(TestType(0.01), 1));
+    REQUIRE_THAT(to_number<TestType>("-1e2").value(), WithinULP(TestType(-100), 1));
+    REQUIRE_THAT(to_number<TestType>("-1e+2").value(), WithinULP(TestType(-100), 1));
+    REQUIRE_THAT(to_number<TestType>("-1.e2").value(), WithinULP(TestType(-100), 1));
+    REQUIRE_THAT(to_number<TestType>("-1.e+2").value(), WithinULP(TestType(-100), 1));
+    REQUIRE_THAT(to_number<TestType>("-1e-2").value(), WithinULP(TestType(-0.01), 1));
+    REQUIRE_THAT(to_number<TestType>("1E2").value(), WithinULP(TestType(100), 1));
+    REQUIRE_THAT(to_number<TestType>("1E+2").value(), WithinULP(TestType(100), 1));
+    REQUIRE_THAT(to_number<TestType>("1E-2").value(), WithinULP(TestType(0.01), 1));
+    REQUIRE_THAT(to_number<TestType>(".1e2").value(), WithinULP(TestType(10), 1));
+    REQUIRE_THAT(to_number<TestType>(".1e+2").value(), WithinULP(TestType(10), 1));
+    REQUIRE_THAT(to_number<TestType>(".1e-2").value(), WithinULP(TestType(0.001), 1));
+    REQUIRE_THAT(to_number<TestType>("0.1e2").value(), WithinULP(TestType(10), 1));
+    REQUIRE_THAT(to_number<TestType>("0.1e+2").value(), WithinULP(TestType(10), 1));
+    REQUIRE_THAT(to_number<TestType>("0.1e-2").value(), WithinULP(TestType(0.001), 1));
+    REQUIRE_THAT(to_number<TestType>("5e-0").value(), WithinULP(TestType(5), 1));
+
+    REQUIRE(to_number<TestType>("").has_value() == false);
+    REQUIRE(to_number<TestType>(" 0.1").has_value() == false);
+    REQUIRE(to_number<TestType>("0.1 ").has_value() == false);
+    REQUIRE(to_number<TestType>(".").has_value() == false);
+    REQUIRE(to_number<TestType>("-.").has_value() == false);
+    REQUIRE(to_number<TestType>("- .1").has_value() == false);
+    REQUIRE(to_number<TestType>("- .1").has_value() == false);
+    REQUIRE(to_number<TestType>("1.2e").has_value() == false);
+    REQUIRE(to_number<TestType>("1e.").has_value() == false);
+    REQUIRE(to_number<TestType>("1e+").has_value() == false);
+    REQUIRE(to_number<TestType>("1e-").has_value() == false);
+}
+
+TEMPLATE_TEST_CASE("to_number(): integer max() values round-trip", "[to_number]",
+                   short, unsigned short, int, unsigned int,
+                   long, unsigned long, long long, unsigned long long)
+{
+    std::stringstream ss;
+    ss << std::setprecision(std::numeric_limits<TestType>::digits10 + 1);
+
+    if (sizeof(TestType) == 1)
+        ss << static_cast<int>(std::numeric_limits<TestType>::max());
+    else 
+        ss << std::numeric_limits<TestType>::max();
+
+    std::string str = ss.str();
+    CAPTURE(str);
+    REQUIRE(to_number<TestType>(str).value() == std::numeric_limits<TestType>::max());
+}
+
+TEMPLATE_TEST_CASE("to_number(): integer lowest() values round-trip", "[to_number]",
+                   short, unsigned short, int, unsigned int,
+                   long, unsigned long, long long, unsigned long long)
+{
+    std::stringstream ss;
+    ss << std::setprecision(std::numeric_limits<TestType>::digits10 + 1);
+
+    if (sizeof(TestType) == 1)
+        ss << static_cast<int>(std::numeric_limits<TestType>::lowest());
+    else
+        ss << std::numeric_limits<TestType>::lowest();
+
+    std::string str = ss.str();
+    CAPTURE(str);
+    REQUIRE(to_number<TestType>(str).value() == std::numeric_limits<TestType>::lowest());
+}
+
+TEST_CASE("to_number(): Overflow for float", "[to_number]")
+{
+    REQUIRE(to_number<float>("3.40282e+38").value() == 3.40282e+38f);
+    REQUIRE(to_number<float>("3.40282e+39").has_value() == false);
+    REQUIRE(to_number<float>("-3.40282e+38").value() == -3.40282e+38f);
+    REQUIRE(to_number<float>("-3.40282e+39").has_value() == false);
+    REQUIRE(to_number<float>("1.17549e-38").value() == 1.17549e-38f);
+    REQUIRE(to_number<float>("1e-50").value() == 0.0f);
 }
 
 /* Disabled because doocsdev16's gcc has insufficient constexpr support (but works on
