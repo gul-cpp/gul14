f82d30da4e43dec1904858e192d7db295561d4ff
Fini Jastrow <ulf.fini.jastrow@desy.de>
2019-12-12 19:16:48 +0100

    examples: Include recommended header "gul14/gul.h" and discard namespace qualifiers
    [why]
    It is bad practice to pollute the global include directory
    with header files, so the previously included ${INCLUDE_PATH}/gul.h
    should be removed at some point. We use "gul14/gul.h" instead. Also,
    the examples are more readable without the namespace qualifiers.
    
    [how]
    Include "gul14/gul.h". Add a number of using directives at the head
    of the example file and remove the gul:: namespace qualifier from
    the trim*() calls.

--- examples/trim.cc
+++ examples/trim.cc
@@ -4,7 +4,7 @@
  * \date   Created on November 22, 2018
  * \brief  Example for the trim() family of functions from the General Utility Library.
  *
- * \copyright Copyright 2018 Deutsches Elektronen-Synchrotron (DESY), Hamburg
+ * \copyright Copyright 2018-2019 Deutsches Elektronen-Synchrotron (DESY), Hamburg
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
@@ -21,39 +21,44 @@
  */
 
 #include <iostream>
-#include <gul.h>
+#include <gul14/gul.h>
 
 using std::cout;
+using gul::trim;
+using gul::trim_left;
+using gul::trim_right;
+using gul::trim_left_sv;
+using gul::trim_right_sv;
 
 int main()
 {
 //! [Using trim() with default and custom whitespace]
-    cout << "[" << gul14::trim("\n \b trim(), default whitespace\t \r") << "]\n";
+    cout << "[" << trim("\n \b trim(), default whitespace\t \r") << "]\n";
     // prints "[trim(), default whitespace]"
 
-    cout << "[" << gul14::trim(".:.:.:trim(), custom whitespace.:.:.:.", ".:") << "]\n";
+    cout << "[" << trim(".:.:.:trim(), custom whitespace.:.:.:.", ".:") << "]\n";
     // prints "[trim(), custom whitespace]"
 //! [Using trim() with default and custom whitespace]
 
 //! [Using trim_left() and trim_right() with default and custom whitespace]
-    cout << "[" << gul14::trim_left("\n \b trim_left(), default whitespace   ") << "]\n";
+    cout << "[" << trim_left("\n \b trim_left(), default whitespace   ") << "]\n";
     // prints "[trim_left(), default whitespace   ]"
 
-    cout << "[" << gul14::trim_right(".:.:.:trim_right(), custom whitespace.:.:.:.", ".:") << "]\n";
+    cout << "[" << trim_right(".:.:.:trim_right(), custom whitespace.:.:.:.", ".:") << "]\n";
     // prints "[.:.:.:trim_right, custom whitespace]"
 //! [Using trim_left() and trim_right() with default and custom whitespace]
 
 //! [Using trim_left_sv() and trim_right_sv() with default and custom whitespace]
-    cout << "[" << gul14::trim_left_sv("\n \b trim_left_sv(), default whitespace   ") << "]\n";
+    cout << "[" << trim_left_sv("\n \b trim_left_sv(), default whitespace   ") << "]\n";
     // prints "[trim_left_sv(), default whitespace   ]"
 
-    cout << "[" << gul14::trim_right_sv(".:.:.:trim_right_sv(), custom whitespace.:.:.:.", ".:") << "]\n";
+    cout << "[" << trim_right_sv(".:.:.:trim_right_sv(), custom whitespace.:.:.:.", ".:") << "]\n";
     // prints "[.:.:.:trim_right_sv, custom whitespace]"
 //! [Using trim_left_sv() and trim_right_sv() with default and custom whitespace]
 
 //! [Using trim_sv() on a mutable string]
     std::string str = "  string_view  ";
-    auto sv = gul14::trim_sv(str); // sv is a string_view that points to the original string
+    auto sv = trim_sv(str); // sv is a string_view that points to the original string
 
     cout << "[" << sv << "]\n";
     // prints "[string_view]"
